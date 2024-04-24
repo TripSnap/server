8ed573452dd7f02a731ac394c6090fa54bd34c1a
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2024-04-24 15:42:42 +0900

    refactor: Friend, FriendRequest 엔티티 수정
    - FriendBaseEntity로 합침

--- api/src/main/java/com/tripsnap/api/domain/entity/base/BaseEntity.java
+++ api/src/main/java/com/tripsnap/api/domain/entity/base/BaseEntity.java
@@ -3,12 +3,16 @@ package com.tripsnap.api.domain.entity.base;
 
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@SuperBuilder(builderMethodName = "baseBuilder")
+@NoArgsConstructor
 public abstract class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
