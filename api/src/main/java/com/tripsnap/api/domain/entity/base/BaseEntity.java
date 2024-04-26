4e27394db89c882788ef6f8210f719809d319207
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2024-04-26 19:13:46 +0900

    fix: jpa auditing 관련 수정

--- api/src/main/java/com/tripsnap/api/domain/entity/base/BaseEntity.java
+++ api/src/main/java/com/tripsnap/api/domain/entity/base/BaseEntity.java
@@ -1,11 +1,14 @@
 package com.tripsnap.api.domain.entity.base;
 
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -13,9 +16,10 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @SuperBuilder(builderMethodName = "baseBuilder")
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
-    @CreatedDate
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
