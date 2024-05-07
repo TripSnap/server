b0bb6904f29efd5cfeec86f66a692f65343be698
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2024-05-07 16:39:35 +0900

    fix: 401 리턴 시 access token 삭제 코드 추가

--- api/src/main/java/com/tripsnap/api/auth/exception/AuthenticationExceptionHandler.java
+++ api/src/main/java/com/tripsnap/api/auth/exception/AuthenticationExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.tripsnap.api.auth.exception;
 
+import com.tripsnap.api.auth.TokenService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -11,12 +13,22 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import java.io.IOException;
 
 public class AuthenticationExceptionHandler implements AuthenticationEntryPoint {
+    private TokenService tokenService;
+
+    @Autowired
+    public void setTokenService(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         if(authException instanceof AccessTokenExpiredException) {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Refresh-Token");
         } else {
+            tokenService.getAccessTokenCookie(request).ifPresent((cookie) -> {
+                tokenService.removeAccessToken(request, response);
+            });
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Bearer");
         }
